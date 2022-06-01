24bf0a8ddb4d371fb240954eaca8edf2ae6b8773
JManuel Velasco <jmanuel.velasco@olympicchannel.com>
2022-06-01 13:12:28 +0200

    WIP: working with style components

--- pages/[name].js
+++ pages/[name].js
@@ -8,7 +8,7 @@
  *  => el parametro de la ruta se obtiene examinando la propiedad params en las props en la función getServerSideProps
  *  => la clave del parámetro es el nombre del archivo: name
  */
-import styled from 'styled-components';
+import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -24,7 +24,6 @@ const Pokemon = (props) => {
       </StyledLink>
 
       <Container>
-
         <div>
           <Image
             src={props.pokemon.sprites.other["official-artwork"].front_default}
@@ -35,34 +34,40 @@ const Pokemon = (props) => {
         </div>
 
         <Properties>
-            <PropertyItem>Height: <span>{props.pokemon.height}</span></PropertyItem>
-            <PropertyItem>Weight: <span>{props.pokemon.weight}</span></PropertyItem>
-            <PropertyItem>Types:</PropertyItem>
-            <Properties>
-              {props.pokemon.types &&
-                props.pokemon.types.map((type, idx) => (
-                  <PropertyItem key={idx}><span>{type.type.name}</span></PropertyItem>
-                ))}
-            </Properties>
-            <PropertyItem>Abilities:</PropertyItem>
-            <Properties>
-              {props.pokemon.abilities &&
-                props.pokemon.abilities.map((abilitie, idx) => (
-                  <PropertyItem key={idx}><span>{abilitie.ability.name}</span></PropertyItem>
-                ))}
-            </Properties>
-            
-            <PropertyItem>Moves:</PropertyItem>
-            <MovesList>
-              {props.pokemon.moves &&
-                props.pokemon.moves.map((move, idx) => (
-                  <MovesItem key={idx}>{move.move.name}</MovesItem>
-                ))}
-            </MovesList>
+          <PropertyItem>
+            Height: <span>{props.pokemon.height}</span>
+          </PropertyItem>
+          <PropertyItem>
+            Weight: <span>{props.pokemon.weight}</span>
+          </PropertyItem>
+          <PropertyItem>Types:</PropertyItem>
+          <Properties>
+            {props.pokemon.types &&
+              props.pokemon.types.map((type, idx) => (
+                <PropertyItem key={idx} pillColor='lightgreen'>
+                  <div>{type.type.name}</div>
+                </PropertyItem>
+              ))}
+          </Properties>
+          <PropertyItem>Abilities:</PropertyItem>
+          <Properties>
+            {props.pokemon.abilities &&
+              props.pokemon.abilities.map((abilitie, idx) => (
+                <PropertyItem key={idx} pillColor='orange'>
+                  <div>{abilitie.ability.name}</div>
+                </PropertyItem>
+              ))}
+          </Properties>
+
+          <PropertyItem>Moves:</PropertyItem>
+          <MovesList>
+            {props.pokemon.moves &&
+              props.pokemon.moves.map((move, idx) => (
+                <MovesItem key={idx}>{move.move.name}</MovesItem>
+              ))}
+          </MovesList>
         </Properties>
-
       </Container>
-
     </>
   );
 };
@@ -90,25 +95,33 @@ const PropertyItem = styled.li`
   font-weight: bold;
   list-style: none;
 
+  div {
+    background-color: ${(props) => props.pillColor};
+    text-align: center;
+    padding: 0.5rem;
+    border-radius: 25px;    
+    font-weight: normal;
+    width: fit-content;
+    margin: 0.1rem 0;
+  }
+
   span {
-    color: #555;
-    font-weight: normal;;
+    font-weight: normal;
   }
 `;
 
 const MovesList = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-gap: 10px;
+  grid-gap: 0.1rem;
 `;
 
 const MovesItem = styled.span`
   background-color: lightblue;
   text-align: center;
   padding: 0.5rem;
-  border-radius: 25px;
+  border-radius: 2.5rem;
 `;
-
 
 export const getServerSideProps = async (ctx) => {
   const queryParam = ctx.params.name;
