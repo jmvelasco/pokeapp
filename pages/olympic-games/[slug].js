c3c8984f21622ac71914fc730711c08d3f829049
JManuel Velasco <jmanuel.velasco@olympicchannel.com>
2022-09-22 20:06:36 +0200

    Include country medals results

--- pages/olympic-games/[slug].js
+++ pages/olympic-games/[slug].js
@@ -9,49 +9,76 @@ const displayDate = (stringDate) => {
   return date.toLocaleDateString();
 };
 
-const OlympicGame = (props) => {
+const OlympicGame = ({ gameDetails }) => {
+  console.dir(gameDetails, { depth: 12 });
   return (
     <>
-      <h1>{props.gameDetails.name}</h1>
+      <h1>{gameDetails.name}</h1>
       <NavigateBack backToUrl="/olympic-games" backToPage="Olympic Games" />
       <Container>
         <CloudinaryImage
-          imageUrl={props.gameDetails.emblem.image.urlTemplate}
+          imageUrl={gameDetails.emblem.image.urlTemplate}
           sources={{
             xs: {
               standard: "t_1-1_380",
               x2: "t_1-1_300",
             },
           }}
-          altText={props.gameDetails.emblem.image.alt}
+          altText={gameDetails.emblem.image.alt}
           data-cy="background-image"
         />
         <Properties>
           <PropertyItem>
             Start of the games: :{" "}
-            <span>{displayDate(props.gameDetails.startDate)}</span>
+            <span>{displayDate(gameDetails.startDate)}</span>
           </PropertyItem>
 
           <PropertyItem>
-            End of the games:{" "}
-            <span>{displayDate(props.gameDetails.endDate)}</span>
+            End of the games: <span>{displayDate(gameDetails.endDate)}</span>
           </PropertyItem>
           <PropertyItem>
             Number of atheletes who participated:{" "}
-            <span>{props.gameDetails.overview.numberOfAthletes}</span>
+            <span>{gameDetails.overview.numberOfAthletes}</span>
           </PropertyItem>
           <PropertyItem>
             Number of countries who participated:{" "}
-            <span>{props.gameDetails.overview.numberOfCountries}</span>
+            <span>{gameDetails.overview.numberOfCountries}</span>
           </PropertyItem>
           <PropertyItem>Sports:</PropertyItem>
           <DisciplinesWrapper>
-            {props.gameDetails.disciplines.map((discipline) => (
+            {gameDetails.disciplines.map((discipline) => (
               <PropertyItem key={discipline.title} pillColor="orange">
                 <div>{discipline.title}</div>
               </PropertyItem>
             ))}
           </DisciplinesWrapper>
+          <PropertyItem>Clasification:</PropertyItem>
+          <AwardsWrapper>
+            {gameDetails.countryAwards.map((awards, idx) => (
+              <>
+                <Awards key={awards.country.name}>
+                  <p>
+                    {idx + 1} - {awards.country.name}
+                  </p>
+
+                  <MedalsWrapper>
+                    {awards.medals.map((medal) => (
+                      <Medal
+                        key={`${medal.medalType}-${medal.count}`}
+                        medalColor={
+                          medal.medalType === "BRONZE"
+                            ? "#CD7F32"
+                            : medal.medalType.toLowerCase()
+                        }
+                      >
+                        {medal.medalType}: {medal.count}
+                      </Medal>
+                    ))}
+                  </MedalsWrapper>
+                </Awards>
+              </>
+            ))}
+          </AwardsWrapper>
         </Properties>
       </Container>
     </>
@@ -62,6 +89,30 @@ const DisciplinesWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 0.1rem;
+`;
+
+const AwardsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1rem;
+`;
+
+const Awards = styled.div`
+  border: 0.1rem solid lightgreen;
+  padding: 1rem;
+
+  > p {
+    font-weight: bold;
+  }
+`;
+
+const MedalsWrapper = styled.div`
+  width: 100%;
+  padding-left: 2rem;
+`;
+
+const Medal = styled.div`
+  background-color: ${(props) => props.medalColor};
 `;
 
 // const Container = styled.div`
